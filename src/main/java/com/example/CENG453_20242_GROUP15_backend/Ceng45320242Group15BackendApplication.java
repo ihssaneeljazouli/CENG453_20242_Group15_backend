c381edbe5fbe6ddcb39dba5e9b693599191f3a01
ihssaneeljazouli <eljazoulii@cy-tech.fr>
2025-04-03 19:13:18 +0200

    springboot initilizer

---
+++ src/main/java/com/example/CENG453_20242_GROUP15_backend/Ceng45320242Group15BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CENG453_20242_GROUP15_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceng45320242Group15BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ceng45320242Group15BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CENG453_20242_GROUP15_backend/Ceng45320242Group15BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CENG453_20242_GROUP15_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ceng45320242Group15BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
