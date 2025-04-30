14895f877d0dc84e5827ae5ef09d6bf28453d99e
ihssaneeljazouli <eljazoulii@cy-tech.fr>
2025-04-30 14:39:25 +0200

    dockerfile and security access

--- src/main/java/com/example/CENG453_20242_GROUP15_backend/config/SecurityConfig.java
+++ src/main/java/com/example/CENG453_20242_GROUP15_backend/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login", "/swagger-ui/**","/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/auth/register", "/auth/login","/","/leaderboard","/leaderboard/all-time","/leaderboard/weekly","/leaderboard/monthly", "/swagger-ui/**","/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
